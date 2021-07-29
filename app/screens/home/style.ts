1395d7c612f7ff84b4afc8d4b389855b4a07abc8
eduardoaraya <eduardojezine@gmail.com>
2021-07-29 00:47:14 -0300

    Add react native types 'styled-components' and change @type/axios from dependences to devDependences

--- app/screens/home/style.ts
+++ app/screens/home/style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 import Colors from '../../components/core/colors';
 
 export const StatusLabel = styled.Text`
